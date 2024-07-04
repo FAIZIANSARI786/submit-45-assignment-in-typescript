c1ffbc67a1e7772e70c10884564ce3804e4c067e
FAIZIANSARI786 <160278769+FAIZIANSARI786@users.noreply.github.com>
2024-07-04 10:53:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// exercise 4
+// let qoute:string ="A person who never made a mistake never trided anything new ";
+// let author : string = "elbert einstine";
+// console.log(`${author}once said,"${qoute}`);
+//exercise 5
+var qoute = "A person who never made a mistake never trided anything new ";
+var famous_person = "elbert einstine";
+var message = "".concat(famous_person, "once said,\"").concat(qoute);
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,11 @@
+// exercise 4
+
+// let qoute:string ="A person who never made a mistake never trided anything new ";
+// let author : string = "elbert einstine";
+// console.log(`${author}once said,"${qoute}`);
+
+//exercise 5
+let qoute:string ="A person who never made a mistake never trided anything new ";
+let famous_person : string = "elbert einstine";
+let message =`${famous_person}once said,"${qoute}`;
+console.log(message);
